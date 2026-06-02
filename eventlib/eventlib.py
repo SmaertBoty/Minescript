3e87b1ad6ffb611c5716f01141357f20ce12d6f9
SmartBoty <boti.varga2009@gmail.com>
2026-06-02 15:46:31 +0200

    totem popped event
    from system.lib.minescript import *
    import eventlib
    from time import sleep
    
    events = EventQueue()
    events.register_totem_popped_listener()
    
    while True:
        event = events.get()
        if event.type == EventType.ENTITY_TOTEM_POPPED:
            echo("Totem popped for: " + event.entity.name)
            execute("/give @p minecraft:totem_of_undying")
            sleep(0.5)
            execute(f"/damage {event.entity.uuid} 20 minecraft:fall")

--- eventlib/eventlib.py
+++ eventlib/eventlib.py
@@ -2,43 +2,57 @@ import system.lib.minescript as m
 from system.lib.minescript import *
 from threading import Thread
 from system.lib.java import eval_pyjinn_script as eps
-import uuid
-
-identifier = uuid.uuid8()
 
 script = eps(
 fr"""
+mc = JavaClass("net.minecraft.client.Minecraft").getInstance()
 ClientboundPlayerChatPacket = JavaClass("net.minecraft.network.protocol.game.ClientboundPlayerChatPacket")
 ClientboundSystemChatPacket = JavaClass("net.minecraft.network.protocol.game.ClientboundSystemChatPacket")
-identifier = "{identifier}"
-if "incoming_intercept_queue" not in __script__.vars["game"]:
-    __script__.vars["game"]["incoming_intercept_queue"] =  {"{"}{"}"}
-if identifier not in __script__.vars["game"]["incoming_intercept_queue"]:
-    __script__.vars["game"]["incoming_intercept_queue"][identifier] = []
+ClientboundEntityEventPacket = JavaClass("net.minecraft.network.protocol.game.ClientboundEntityEventPacket")
+System = JavaClass("java.lang.System")
+incoming_intercept_queue = []
+totem_popped_queue = []
 def get_incoming_intercept_queue():
-    out = __script__.vars["game"]["incoming_intercept_queue"][identifier]
-    __script__.vars["game"]["incoming_intercept_queue"][identifier] = []
+    global incoming_intercept_queue
+    out = incoming_intercept_queue
+    incoming_intercept_queue = []
+    return str(out)
+def get_totem_popped_queue():
+    global totem_popped_queue
+    out = totem_popped_queue
+    totem_popped_queue = []
     return str(out)
 def s2c(event):
     if isinstance(event.packet, ClientboundSystemChatPacket):
-        __script__.vars["game"]["incoming_intercept_queue"][identifier].append(event.packet.content().getString())
+        incoming_intercept_queue.append(event.packet.content().getString())
         event.cancel()
     elif isinstance(event.packet, ClientboundPlayerChatPacket):
-        try: __script__.vars["game"]["incoming_intercept_queue"][identifier].append(event.packet.unsignedContent().getString())
-        except: __script__.vars["game"]["incoming_intercept_queue"][identifier].append(event.packet.body().content())
+        try: incoming_intercept_queue.append(event.packet.unsignedContent().getString())
+        except: incoming_intercept_queue.append(event.packet.body().content())
         event.cancel()
+    elif isinstance(event.packet, ClientboundEntityEventPacket):
+        if int(event.packet.getEventId()) == 35: totem_popped_queue.append(event.packet.getEntity(mc.level).getStringUUID())
 add_event_listener("clientbound_packet",s2c)
 """)
 
 get_incoming_intercept_queue = script.get("get_incoming_intercept_queue")
+get_totem_popped_queue = script.get("get_totem_popped_queue")
 
 class INCOMING_CHAT_INTERCEPT:
     def __init__(self,type,message):
         self.type = type
         self.message = message
 
+class ENTITY_TOTEM_POPPED:
+    def __init__(self,type,entity):
+        self.type = type
+        self.entity = entity
+
 m.EventType.INCOMING_CHAT_INTERCEPT = "incoming_chat_intercept"
 m._EVENT_CONSTRUCTORS["incoming_chat_intercept"] = INCOMING_CHAT_INTERCEPT
+
+m.EventType.ENTITY_TOTEM_POPPED = "entity_totem_popped"
+m._EVENT_CONSTRUCTORS["entity_totem_popped"] = ENTITY_TOTEM_POPPED
 
 def register_incoming_chat_interceptor(self):
     def worker():
@@ -51,4 +65,16 @@ def register_incoming_chat_interceptor(self):
                 })
     Thread(target=worker, daemon=True).start()
 
+def register_totem_popped_listener(self):
+    def worker():
+        while True:
+            events = eval(get_totem_popped_queue())
+            for event in events:
+                self.queue.put({
+                    "type": m.EventType.ENTITY_TOTEM_POPPED,
+                    "entity": [e for e in entities() if e.uuid == event][0]
+                })
+    Thread(target=worker, daemon=True).start()
+
 setattr(m.EventQueue, "register_incoming_chat_interceptor", register_incoming_chat_interceptor)
+setattr(m.EventQueue, "register_totem_popped_listener", register_totem_popped_listener)
